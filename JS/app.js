3641aa3fcd1c2461413fe5be61a401bf5ee5b790
Francisco <106126465+Francisco-Carusso@users.noreply.github.com>
2022-10-05 17:51:20 -0300

    Todointerconectado V1.1.0 Conexión con el menu

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,4 @@
+// Section menu
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
@@ -8,3 +9,33 @@ function toggleMenu() {
 
 openMenuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
+
+// sacar el menú al seleccionar una sección - bring up the menu when selecting a section
+// Combinación de teclado:
+// alt + 91: []  -  alt + 92: \  -  alt + 94: ^  - alt + 96: ``
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        const id = entry.target.getAttribute("id");
+        const menuLink = document.querySelector(`.menu a[href="#${id}"]`);
+
+        if(entry.isIntersecting) {
+            document.querySelector(".menu a.selected").classList.remove("selected");
+            menuLink.classList.add("selected");
+        }
+    });
+}, {rootMargin: "-40% 0px -60% 0px"});
+
+menuLinks.forEach(menuLink => {
+    menuLink.addEventListener("click", function() {
+        menu.classList.remove("menu_opened");
+    });
+
+    const hash = menuLink.getAttribute("href");
+    const target = document.querySelector(hash);
+    if(target) {
+        observer.observe(target);
+    }
+});
+
