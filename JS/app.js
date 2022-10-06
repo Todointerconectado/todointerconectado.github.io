d53301e6652df92e583cb3f61e5aa0f20813c8a6
Francisco <106126465+Francisco-Carusso@users.noreply.github.com>
2022-10-06 19:44:18 -0300

    Todointerconectado V1.1.2 form in process

--- JS/app.js
+++ JS/app.js
@@ -39,3 +39,20 @@ menuLinks.forEach(menuLink => {
     }
 });
 
+// connect the form to whatsapp
+
+// function gotowhatsapp() {
+    
+//     var name = document.getElementById("name").value;
+//     var phone = document.getElementById("phone").value;
+//     var email = document.getElementById("email").value;
+//     var service = document.getElementById("service").value;
+
+//     var url = "https://wa.me/918789529215?text=" 
+//     + "Name: " + name + "%0a"
+//     + "Phone: " + phone + "%0a"
+//     + "Email: " + email  + "%0a"
+//     + "Service: " + service; 
+
+//     window.open(url, '_blank').focus();
+// }
